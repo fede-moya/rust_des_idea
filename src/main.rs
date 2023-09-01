e7b068098cd271af98c3f8bbc4c17c212956756a
Federico Moyá <federicomoyamartin@gmail.com>
2023-09-01 14:55:24 -0300

    not so ugly

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
 use serde::Deserialize;
 use std::collections::HashMap;
 use toml;
+
+// Create a new struct `Cars` to encapsulate the HashMap
+#[derive(Debug)]
+struct Cars {
+    data: HashMap<String, Car>,
+}
+
+// Implement `Deserialize` for `Cars` to handle the deserialization logic
+impl<'de> Deserialize<'de> for Cars {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let mut data = HashMap::<String, Car>::deserialize(deserializer)?;
+
+        // Iterate over the HashMap and set the name for each car based on its key
+        for (key, mut car) in data.iter_mut() {
+            car.name = Some(key.clone());
+        }
+
+        Ok(Cars { data })
+    }
+}
+
 #[derive(Debug, Deserialize)]
 struct Toys {
     balls: u8,
     bricks: u8,
-    cars: HashMap<String, Car>,
+    cars: Cars,
     trucks: HashMap<String, Truck>,
 }
 
 #[derive(Debug, Deserialize)]
 struct Car {
+    name: Option<String>,  // Make the name field optional
     color: String,
     speed: u8,
 }
